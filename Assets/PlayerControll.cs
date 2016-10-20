42fbacc1cc8f1a93c2c6bac34d19d908608f1e61
Denis Kliuch <denysko.kl@gmail.com>
2016-10-20 17:01:50 +0300

    add sprites

---
+++ Assets/PlayerControll.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+public class PlayerControll : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler {
+
+
+
+//	public void OnPointerDown (PointerEventData eventData)
+//	{
+//		
+//	}
+//
+}
